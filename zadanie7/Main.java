306a9a6e98ac2251afd0f8e9fe6e0aa9d8266f81
WojtixS <91822459+WojtixS@users.noreply.github.com>
2021-10-10 15:23:55 +0200

    Add files via upload

---
+++ zadanie7/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println(sqrt(8));
+
+
+    }
+
+
+    private static double sqrt(double numer) {
+        return Math.sqrt(numer);
+    }
+}
